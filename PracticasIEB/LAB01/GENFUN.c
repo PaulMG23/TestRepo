e8bee4cadf5914a7170539f457fc42540b4596e7
Paul <paul-mg@hotmail.com>
2020-10-02 08:40:56 -0500

    CapsOff Working

---
+++ PracticasIEB/LAB01/GENFUN.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include "GENFUN.h"
+
+void main ( void )
+{
+	printf("Testing GENFUN_vCapsOff\n");
+	uint8 au8BufferTest[12] = "HELLO WoRLD!";
+	printf("Buffer before function %s\n", au8BufferTest);
+	GENFUN_vCapsOff(&au8BufferTest[0], 12);
+	printf("Buffer after function %s\n", au8BufferTest);
+
+
+}
+
+void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList) 
+{
+	while ( u8SizeOfList != 0 )
+	{
+		if( *pu8Src >= ASCII_LOW_THRESHOLD_CAPSON_TO_OFF &&
+			*pu8Src <= ASCII_HIGH_THRESHOLD_CAPSON_TO_OFF )
+		{
+			*pu8Src += ASCII_CONVERTION_FACTOR;
+		}
+		else
+		{
+			/*Nothing to do*/
+		}
+		pu8Src++;
+		u8SizeOfList --;
+	}
+}
---
+++ PracticasIEB/LAB01/GENFUN.h
@@ -0,0 +1,14 @@
+
+/*Data Types*/
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned int   uint32;
+
+/*Prototypes*/
+void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList) ;
+
+/*Definitions*/
+#define ASCII_HIGH_THRESHOLD_CAPSON_TO_OFF		90
+#define ASCII_LOW_THRESHOLD_CAPSON_TO_OFF		65
+#define ASCII_CONVERTION_FACTOR					32
+
